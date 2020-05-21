80cf400edca15493e42c44f20d2a4620c3ec75e9
leanonybr-7579 <ulissescarvalho.d@gmail.com>
2020-05-20 21:11:39 -0300

    nah

--- src/components/license.jsx
+++ src/components/license.jsx
@@ -4,7 +4,7 @@ export default function License() {
     return (
         <div style={{ marginTop: 80 }}>
             <h1>License</h1>
-            <p>All images were made by freepik, fullvector</p>
+            <p>All images were made by freepik, fullvector and dgim-studio</p>
             <p>404 Error: <a href="https://www.freepik.com/free-photos-vectors/background">Background vector created by freepik - www.freepik.com</a></p>
             <p>Homepage image: <a href="https://www.freepik.com/free-photos-vectors/abstract">Abstract vector created by fullvector - www.freepik.com</a></p>
             <p>Projects page: <a href="https://www.freepik.com/free-photos-vectors/abstract">Abstract vector created by fullvector - www.freepik.com</a></p>
